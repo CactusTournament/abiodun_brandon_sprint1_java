af623bac3f5a0e4ed858773ae6273c5b402d661f
Magret <belloabiodun17@gmail.com>
2025-10-21 00:42:03 -0230

    Added patient's tests

---
+++ PatientTest.java
@@ -0,0 +1,20 @@
+public class PatientTest {
+    public static void main(String[] args) {
+        // Create a patient
+        Patient patient = new Patient(1, "John Doe", 30, "555-1234");
+
+        // Access patient properties
+        System.out.println("\n");
+        System.out.println("_____Patient Properties:______");
+        System.out.println(patient.getPatientID());
+        System.out.println(patient.getName());
+        System.out.println(patient.getAge());
+        System.out.println(patient.getPhoneNumber());
+
+        // Display patient information
+        System.out.println("\n\n");
+        System.out.println("_____Patient Information:______");
+        System.out.println(patient);
+        System.out.println("\n");
+    }
+}
